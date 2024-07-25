fdfa2fceee9f1bd6d9267c5feb985319ccdbf53d
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-07-26 00:05:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,7 +84,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/OtakuFlix_Network/4639')
+                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/+JFzZgLbzmGNlNjk9')
                 ]
             ]
                 )
